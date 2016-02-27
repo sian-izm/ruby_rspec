771dc68f93e0ef14d21516c18d055e8a71f42e41
sian-izm <shian.izumida@peroli.jp>
2016-02-27 16:02:46 +0900

    contain対象のgrid_pointの変数名を分かりやすくした

--- spec/lib/grid_points_spec.rb
+++ spec/lib/grid_points_spec.rb
@@ -5,27 +5,28 @@ describe GridPoints do
     context "格子点集合に含まれる場合" do
       let(:grid_point_1) { GridPoint.new(4, 7) }
       let(:grid_point_2) { GridPoint.new(3, 8) }
+      let(:target_grid_point) { GridPoint.new(4, 7) }
 
       before do
         grid_points.add(grid_point_1)
         grid_points.add(grid_point_2)
       end
 
-      subject { grid_points.contains?(grid_point_1) }
+      subject { grid_points.contains?(target_grid_point) }
       it { is_expected.to be_truthy }
     end
 
     context "格子点集合に含まれない場合" do
       let(:grid_point_1) { GridPoint.new(4, 7) }
       let(:grid_point_2) { GridPoint.new(3, 8) }
-      let(:grid_point_3) { GridPoint.new(5, 1) }
+      let(:target_grid_point) { GridPoint.new(5, 1) }
 
       before do
         grid_points.add(grid_point_1)
         grid_points.add(grid_point_2)
       end
 
-      subject { grid_points.contains?(grid_point_3) }
+      subject { grid_points.contains?(target_grid_point) }
       it { is_expected.to be_falsy }
     end
   end
