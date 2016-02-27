65c8b903741948f28368ca05fae78f8409b655bf
sian-izm <shian.izumida@peroli.jp>
2016-02-27 16:03:35 +0900

    初期データを共通化

--- spec/lib/grid_points_spec.rb
+++ spec/lib/grid_points_spec.rb
@@ -2,9 +2,10 @@ describe GridPoints do
   let(:grid_points) { GridPoints.new() }
 
   describe "#contains?" do
+    let(:grid_point_1) { GridPoint.new(4, 7) }
+    let(:grid_point_2) { GridPoint.new(3, 8) }
+
     context "格子点集合に含まれる場合" do
-      let(:grid_point_1) { GridPoint.new(4, 7) }
-      let(:grid_point_2) { GridPoint.new(3, 8) }
       let(:target_grid_point) { GridPoint.new(4, 7) }
 
       before do
@@ -17,8 +18,6 @@ describe GridPoints do
     end
 
     context "格子点集合に含まれない場合" do
-      let(:grid_point_1) { GridPoint.new(4, 7) }
-      let(:grid_point_2) { GridPoint.new(3, 8) }
       let(:target_grid_point) { GridPoint.new(5, 1) }
 
       before do
