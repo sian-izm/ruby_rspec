b3ee0f8b7e34735c9e41b7c195956f4bf485cf34
sian-izm <shian.izumida@peroli.jp>
2016-02-27 13:56:15 +0900

    x,y をletに切り出した

--- spec/lib/grid_point_spec.rb
+++ spec/lib/grid_point_spec.rb
@@ -1,30 +1,42 @@
 describe GridPoint do
 
   describe "#x" do
+    let(:x) { 4 }
+    let(:y) { 7 }
+
     it do
-      obj = GridPoint.new(4, 7)
+      obj = GridPoint.new(x, y)
       expect(obj.x).to eq(4)
     end
   end
 
   describe "#y" do
+    let(:x) { 4 }
+    let(:y) { 7 }
+
     it do
-      obj = GridPoint.new(4, 7)
+      obj = GridPoint.new(x, y)
       expect(obj.y).to eq(7)
     end
   end
 
   describe "#notation" do
     context "xが4, yが7" do
+      let(:x) { 4 }
+      let(:y) { 7 }
+
       it do
-        obj = GridPoint.new(4, 7)
+        obj = GridPoint.new(x, y)
         expect(obj.notation).to eq("(4,7)")
       end
     end
 
     context "xが1, yが2" do
+      let(:x) { 1 }
+      let(:y) { 2 }
+
       it do
-        obj = GridPoint.new(1, 2)
+        obj = GridPoint.new(x, y)
         expect(obj.notation).to eq("(1,2)")
       end
     end
