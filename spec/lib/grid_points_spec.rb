f84880a9db1c1e8c86f564e4f44ad18ee3a4c47c
sian-izm <shian.izumida@peroli.jp>
2016-02-27 15:56:36 +0900

    格子点集合に含まれないテストケースを追加

--- spec/lib/grid_points_spec.rb
+++ spec/lib/grid_points_spec.rb
@@ -2,14 +2,29 @@ describe GridPoints do
   let(:grid_points) { GridPoints.new() }
 
   describe "#contains?" do
-    let(:grid_point_1) { GridPoint.new(4, 7) }
-    let(:grid_point_2) { GridPoint.new(3, 8) }
+    context "格子点集合に含まれる場合" do
+      let(:grid_point_1) { GridPoint.new(4, 7) }
+      let(:grid_point_2) { GridPoint.new(3, 8) }
 
-    before do
-      grid_points.add(grid_point_1)
-      grid_points.add(grid_point_2)
+      before do
+        grid_points.add(grid_point_1)
+        grid_points.add(grid_point_2)
+      end
+
+      it { expect(grid_points.contains?(grid_point_1)).to be_truthy }
     end
-    
-    it { expect(grid_points.contains?(grid_point_1)).to be_truthy }
+
+    context "格子点集合に含まれない場合" do
+      let(:grid_point_1) { GridPoint.new(4, 7) }
+      let(:grid_point_2) { GridPoint.new(3, 8) }
+      let(:grid_point_3) { GridPoint.new(5, 1) }
+
+      before do
+        grid_points.add(grid_point_1)
+        grid_points.add(grid_point_2)
+      end
+
+      it { expect(grid_points.contains?(grid_point_3)).to be_falsy }
+    end
   end
 end
