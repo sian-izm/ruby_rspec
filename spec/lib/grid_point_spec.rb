da23b845150bbc9ad54d93f70f7af065c8a29668
sian-izm <shian.izumida@peroli.jp>
2016-02-27 14:05:04 +0900

    subjectを使って見やすくした

--- spec/lib/grid_point_spec.rb
+++ spec/lib/grid_point_spec.rb
@@ -5,18 +5,16 @@ describe GridPoint do
     let(:x) { 4 }
     let(:y) { 7 }
 
-    it do
-      expect(grid_point.x).to eq(4)
-    end
+    subject { grid_point.x }
+    it { is_expected.to eq 4 }
   end
 
   describe "#y" do
     let(:x) { 4 }
     let(:y) { 7 }
 
-    it do
-      expect(grid_point.y).to eq(7)
-    end
+    subject { grid_point.y }
+    it { is_expected.to eq 7 }
   end
 
   describe "#notation" do
@@ -24,18 +22,16 @@ describe GridPoint do
       let(:x) { 4 }
       let(:y) { 7 }
 
-      it do
-        expect(grid_point.notation).to eq("(4,7)")
-      end
+      subject { grid_point.notation }
+      it { is_expected.to eq "(4,7)" }
     end
 
     context "xが1, yが2" do
       let(:x) { 1 }
       let(:y) { 2 }
 
-      it do
-        expect(grid_point.notation).to eq("(1,2)")
-      end
+      subject { grid_point.notation }
+      it { is_expected.to eq "(1,2)" }
     end
   end
 end
