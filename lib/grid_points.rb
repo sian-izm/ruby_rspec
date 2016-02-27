b08fb3adb7139a0d1445f23d2c5eb27633925bcc
sian-izm <shian.izumida@peroli.jp>
2016-02-27 16:37:17 +0900

    add, initializeで配列,オブジェクトのどちらも利用できるようにした

--- lib/grid_points.rb
+++ lib/grid_points.rb
@@ -2,14 +2,27 @@ class GridPoints
   attr_accessor :grid_points
 
   def initialize(grid_points = [])
-    @grid_points = grid_points
+    @grid_points = array_grid_points(grid_points)
   end
 
-  def add(grid_point)
-    @grid_points << grid_point
+  def add(grid_points)
+    @grid_points + array_grid_points(grid_points)
   end
 
   def contains?(grid_point)
     @grid_points.map { |i| i.same_coordinates?(grid_point) }.include?(true)
+  end
+
+  private
+
+  def array_grid_points(grid_points)
+    case
+    when grid_points.instance_of?(Array)
+      grid_points
+    when grid_points.instance_of?(GridPoint)
+      [grid_points]
+    else
+      raise StandardError
+    end
   end
 end
--- spec/lib/grid_points_spec.rb
+++ spec/lib/grid_points_spec.rb
@@ -20,4 +20,44 @@ describe GridPoints do
       it { is_expected.to be_falsy }
     end
   end
+
+  describe "#add" do
+    let(:initial_grid_points) { [GridPoint.new(1, 1)] }
+
+    context "引数が配列のとき" do
+      let(:grid_point_1) { GridPoint.new(4, 7) }
+      let(:grid_point_2) { GridPoint.new(3, 8) }
+      let(:grid_point_3) { GridPoint.new(4, -1) }
+      let(:arg) { [grid_point_1, grid_point_2, grid_point_3] }
+
+      subject { grid_points.add arg }
+      it { is_expected.to include(grid_point_1, grid_point_2, grid_point_3) }
+    end
+
+    context "引数がGridPointインスタンスのとき" do
+      let(:arg) { GridPoint.new(2, 9) }
+
+      subject { grid_points.add arg }
+      it { is_expected.to include(arg) }
+    end
+
+    context "引数が文字列のとき" do
+      let(:arg) { "foobar" }
+
+      it { expect { grid_points.add arg }.to raise_error StandardError }
+    end
+
+    context "引数が空配列のとき" do
+      let(:arg) { [] }
+
+      subject { grid_points.add arg }
+      it { is_expected.to eq initial_grid_points }
+    end
+
+    context "引数がnilのとき" do
+      let(:arg) { nil }
+
+      it { expect { grid_points.add arg }.to raise_error StandardError }
+    end
+  end
 end
